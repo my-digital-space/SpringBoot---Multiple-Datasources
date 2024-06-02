ff1e798a2a30f53017c1718ba1e7e11f6a00224c
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-06-02 21:39:13 +0530

    initial commit

---
+++ SBMultipleDBs/src/main/java/com/demo/multipledb/SbMultipleDBsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.multipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMultipleDBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMultipleDBsApplication.class, args);
+	}
+
+}
---
+++ SBMultipleDBs/src/test/java/com/demo/multipledb/SbMultipleDBsApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.demo.multipledb;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SbMultipleDBsApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}

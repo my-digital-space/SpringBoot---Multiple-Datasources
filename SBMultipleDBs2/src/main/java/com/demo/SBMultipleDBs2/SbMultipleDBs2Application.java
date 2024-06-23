05412a65f6e5a82f0a536aa57bd3ecb2ca9678ca
prosenjit005 <mydigitalspace.pro@gmail.com>
2024-06-23 16:18:54 +0530

    initial commit

---
+++ SBMultipleDBs2/src/main/java/com/demo/SBMultipleDBs2/SbMultipleDBs2Application.java
@@ -0,0 +1,13 @@
+package com.demo.SBMultipleDBs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMultipleDBs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMultipleDBs2Application.class, args);
+	}
+
+}
